c4c69dcaff0e2f1752cbf0e27d72f712b32f2e28
github username <github email address>
2019-05-21 19:14:35 -0400

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -4,9 +4,9 @@ const POKEMONS_URL = `${BASE_URL}/pokemons`
 const main = document.querySelector('main');
 const newPokemon = document.querySelector('#pokemon-form');
 const labelForm = document.querySelector('#trainer-name');
-//let currentTrainer = null;
 let currentPokemon = null; //presents id for clarity
-
+let currentTrainer = null;
+let trainerElement = null;
 
 document.addEventListener('DOMContentLoaded',function(){
     console.log('loaded');
@@ -15,11 +15,8 @@ document.addEventListener('DOMContentLoaded',function(){
     //form wasn't needed. db handles any post requests and returns random pokemon
 })
 
-function addPokemon(trainer){
-    //e.preventDefault();
-    // let nickname = document.querySelector('#nickname').value;
-    // let species = document.querySelector('#species').value;
-    const currentTrainer = trainer;
+function addPokemon(){
+ 
     fetch(`http://localhost:3000/pokemons/`,{
         method: "POST",
         headers: {
@@ -35,90 +32,33 @@ function addPokemon(trainer){
             // }
           })
     })
-    // .then(resp => resp.json())
-    // .then(data => console.log(data))
-    loadAllTrainers();
-    // console.log(currentTrainer);
-    // console.log(currentTrainer.id);
+     .then(resp => resp.json())
+     .then(data => addPokemonToList(data));
 }
 
 function release(){
     fetch(`http://localhost:3000//pokemons/${currentPokemon}`,{
         method: "DELETE"
     })
+    pokemonElement.remove();
     // .then(resp => resp.json())
     // .then(data => console.log(data))
     //loadAllTrainers();
-   
 }
 
 function loadAllTrainers(){
-
-    while (main.firstChild) {
-        main.removeChild(main.firstChild);
-    }
+    main.innerHTML = "";
     fetch('http://localhost:3000/trainers')
     .then(resp => resp.json())
     .then(data => {
-        createElements(data);
-        // data.forEach(trainer => {
-        //     let div = document.createElement('div');
-        //     div.className = 'card';
-        //     div.setAttribute('data-id', trainer.id);
-        //     let p = document.createElement('p');
-        //     p.innerText = trainer.name;
-        //     let button = document.createElement('button');
-        //     button.setAttribute("data-trainer-id",trainer.id)
-        //     button.innerText = 'Add Pokemon';
-        //     button.addEventListener('click',function(e){
-        //         // console.log('clicked')
-        //         // console.log(e);
-        //         //check current trainer pokemon length
-        
-        //         // console.log(trainer.id);
-        //         // console.log(trainer.pokemons);
-        //         // console.log(trainer.pokemons.length);
-        //         //labelForm.innerText = trainer.name;
-                
-        //         //stored in global scope
-        //         currentTrainer = trainer;
-        //         //console.log(trainer);
-        //         //call post method
-        //         addPokemon();
-        //         //reloads page to see update
-        //         document.location.reload(true)
-        
-        //     });
-        //     let ul = document.createElement('ul');
-        //     trainer.pokemons.forEach(pokemon => {
-        //         let li = document.createElement('li');
-        //         li.innerText = `${pokemon.nickname} (${pokemon.species})`;
-        //         let pokemonButton = document.createElement('button');
-        //         pokemonButton.innerText = "Release";
-        //         pokemonButton.className = "release";
-        //         pokemonButton.setAttribute('data-pokemon-id',pokemon.id);
-        //         pokemonButton.addEventListener('click',function(){
-        //             //stored in global scope
-        //             currentPokemon = pokemon.id
-        //             //call delete method
-        //             release();
-        //             //reload page
-        //             document.location.reload(true)
-        //         })
-        //         li.appendChild(pokemonButton);
-        //         ul.appendChild(li);
-        //     })
-        //     div.appendChild(p);
-        //     div.appendChild(button);
-        //     div.appendChild(ul);
-        //     main.appendChild(div);
-        //     //console.log(trainer.pokemons);
-        // })
+        data.forEach(trainer => {
+            createElements(trainer);
+        })   
     })
 }
 
-function createElements(data){
-    data.forEach(trainer => {
+function createElements(trainer){
+    
         let div = document.createElement('div');
         div.className = 'card';
         div.setAttribute('data-id', trainer.id);
@@ -128,22 +68,16 @@ function createElements(data){
         button.setAttribute("data-trainer-id",trainer.id)
         button.innerText = 'Add Pokemon';
         button.addEventListener('click',function(e){
-            // console.log('clicked')
-            // console.log(e);
-            //check current trainer pokemon length
-    
-            // console.log(trainer.id);
-            // console.log(trainer.pokemons);
-            // console.log(trainer.pokemons.length);
-            //labelForm.innerText = trainer.name;
             
-            //stored in global scope
-            //currentTrainer = trainer;
-            //console.log(trainer);
-            //call post method
-            addPokemon(trainer);
-            //reloads page to see update
-            //document.location.reload(true)
+            currentTrainer = trainer;
+            trainerElement = ul
+            if(trainerElement.childNodes.length < 6){
+                addPokemon();
+            }
+            else{
+                alert('TOOOOOO MUCHH POKE')
+            }
+            //console.log(trainerElement.childNodes.length);
     
         });
         let ul = document.createElement('ul');
@@ -158,10 +92,9 @@ function createElements(data){
                 //stored in global scope
                 currentPokemon = pokemon.id
                 //call delete method
+                pokemonElement = li;
                 release();
-                
-                //reload page
-                document.location.reload(true)
+
             })
             li.appendChild(pokemonButton);
             ul.appendChild(li);
@@ -171,5 +104,25 @@ function createElements(data){
         div.appendChild(ul);
         main.appendChild(div);
         //console.log(trainer.pokemons);
-    })
+}
+
+function addPokemonToList(pokemon){
+    let li = document.createElement('li');
+    li.innerText = `${pokemon.nickname} (${pokemon.species})`;
+    let pokemonButton = document.createElement('button');
+    pokemonButton.innerText = "Release";
+    pokemonButton.className = "release";
+    pokemonButton.setAttribute('data-pokemon-id',pokemon.id);
+    pokemonButton.addEventListener('click',function(){
+                //stored in global scope
+                
+        currentPokemon = pokemon.id
+                //call delete method
+        trainerElement = li;
+        
+        console.log(trainerElement);
+        release();
+        })
+    li.appendChild(pokemonButton);
+    trainerElement.appendChild(li);
 }
